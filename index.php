a895276545356f7b7528a8c42dea591c692ab66f
Piotr Smach <p.smach@dicella.com>
2022-06-13 22:31:22 +0200

    started working on routing

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-echo "Hello there";
+require 'Routing.php';
 
-?>
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::run($path);
