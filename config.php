1902970c511334d5a52c46d8c763b0ae0cdfd45d
Piotr Smach <p.smach99@gmail.com>
2022-06-25 18:44:56 +0200

    testing github action to deploy an app to heroku

--- config.php
+++ config.php
@@ -1,1 +1,9 @@
 
+<?php
+
+define("USERNAME", $_ENV["DB_USERNAME"]);
+define("PASSWORD", $_ENV["DB_PASSWORD"]);
+define("HOST", $_ENV["DB_HOST"]);
+define("DATABASE", $_ENV["DATABASE"]);
+
+define("GGL_API", $_ENV["GOOGLE_API"]);
